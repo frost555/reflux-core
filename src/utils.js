352b8c3311e9ec53b59df87daa7af60be875b676
Окороков И. А <frost555@gmail.com>
2015-11-02 14:46:52 +0300

    используется lodash

--- src/utils.js
+++ src/utils.js
@@ -11,28 +11,16 @@ export function callbackName(string, prefix) {
  * isObject, extend, isFunction, isArguments are taken from undescore/lodash in
  * order to remove the dependency
  */
+
 export function isObject(obj) {
     const type = typeof obj;
     return type === "function" || type === "object" && !!obj;
 }
 
+var lodashAssign = require('lodash.assign');
+
 export function extend(obj) {
-    if (!isObject(obj)) {
-        return obj;
-    }
-    var source, prop;
-    for (var i = 1, length = arguments.length; i < length; i++) {
-        source = arguments[i];
-        for (prop in source) {
-            if (Object.getOwnPropertyDescriptor && Object.defineProperty) {
-                var propertyDescriptor = Object.getOwnPropertyDescriptor(source, prop);
-                Object.defineProperty(obj, prop, propertyDescriptor);
-            } else {
-                obj[prop] = source[prop];
-            }
-        }
-    }
-    return obj;
+    return lodashAssign(obj);
 }
 
 export function isFunction(value) {
